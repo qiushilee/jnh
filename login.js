14e29ac2437f81988c04b184fec654c346cdf4c3
lee <qiushi@taotaosou.com>
2014-06-30 17:42:10 +0800

    添加登录为空验证，添加回车键登录支持

--- login.js
+++ login.js
@@ -1,7 +1,7 @@
 Ext.application({
   name: "JNH",
   launch: function() {
-  	Ext.create("Ext.form.Panel", {
+  	var form = Ext.create("Ext.form.Panel", {
       title: "登录",
       layout: "hbox",
       bodyPadding: 10,
@@ -11,16 +11,39 @@ Ext.application({
       border: 0,
       url: env.services.web + env.api.uc.login,
       method: "POST",
+      defaults: {
+        listeners : {
+          specialkey : function(field, e) {
+            if (e.getKey() == Ext.EventObject.ENTER) {
+              var form = this.up('form').getForm();
+              if (form.isValid()) {
+                form.submit({
+                  success: function() {
+                    location.href = "index.html";
+                  },
+                  failure: function(form, action) {
+                    Ext.Msg.alert("登录失败", action.result.errors.msg, function() {
+                      location.href = "login.html";
+                    });
+                  }
+                });
+              }
+            }
+          }
+        }
+      },
       items: [
         {
           fieldLabel: "用户名",
           name: "username",
+          allowBlank:false,
           labelWidth: 40,
           width: 120,
           labelAlign: "right"
         }, {
           inputType: "password",
           name: "password",
+          allowBlank:false,
           fieldLabel: "密码",
           labelWidth: 40,
           width: 120,
@@ -47,5 +70,7 @@ Ext.application({
         }
       ]
   	});
+
+    form.getForm().findField("username").focus();
   }
 });
