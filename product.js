8f0010c8a20230dcefb4a621f0ff4c6104b7e199
qiushi <showbei@gmail.com>
2014-12-17 16:14:20 +0800

    修改打印参数

--- product.js
+++ product.js
@@ -507,7 +507,9 @@ Ext.application({
     window.printHandle.get({
       $el: Ext.ComponentQuery.query("[itemId=product]")[0],
       type: "productrecord",
-      margin: "20 0 0 20"
+      margin: "20 0 0 20",
+      form: Ext.ComponentQuery.query("[itemId=search-bar]")[0].getForm(),
+      title: Ext.ComponentQuery.query("[itemId=productList]")[0].title
     });
 
     window.printHandle.get({
